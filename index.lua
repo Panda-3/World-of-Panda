6def038ca64faf1d78515fbecbefd86310030314
Panda-3 <Panda-3@pandamail.com>
2016-10-30 14:01:42 +0000

    Fixed Wording

--- index.lua
+++ index.lua
@@ -2,7 +2,7 @@
 white = Color.new(255,255,255)
 
 --Font/BG
-fontb = Font.load("app0:/Roboto.ttf")
+robotofont = Font.load("app0:/Roboto.ttf")
 BG = Graphics.loadImage("app0:/BG.png")
 --text location
 x=5
@@ -10,9 +10,9 @@ q=15
 --------------------------------------------------- functions
  function ScreenDraw ()
 	Graphics.drawImage(0,0,BG)
-	Font.setPixelSizes(fontb,35)
-	Font.print(fontb, 480, 5, "Panda says Hello World!", Color.new(64,64,64))
-	Font.setPixelSizes(fontb,18)
+	Font.setPixelSizes(robotofont,35)
+	Font.print(robotofont, 480, 5, "World of Panda", Color.new(64,64,64))
+	Font.setPixelSizes(robotofont,18)
 end
 
 while true do
@@ -22,23 +22,23 @@ y=45
 Graphics.initBlend()
 ScreenDraw()
 
-Font.print(fontb, x, y, tostring(System.getBatteryPercentage()) .. "% Battery gonna BLOWWWW", white)
+Font.print(robotofont, x, y, tostring(System.getBatteryPercentage()) .. "% Battery gonna BLOWWWW", white)
 y=y+q
-Font.print(fontb, x, y, tostring(System.getCpuSpeed()) .. "MHZ SPEEEEEED", white)
+Font.print(robotofont, x, y, tostring(System.getCpuSpeed()) .. "MHZ SPEEEEEED", white)
 y=y+q
-Font.print(fontb,x , y, "PRESS X to make magic happen Pls DONT! do it save me from Panda", white)
+Font.print(robotofont,x , y, "PRESS X to make magic happen Pls DONT! do it save me from Panda", white)
 y=y+q
-Font.print(fontb,x , y, "dont do it save me from Panda", white)
+Font.print(robotofont,x , y, "dont do it save me from Panda", white)
 y=y+q
-Font.print(fontb,x , y, "He is comming SAVE ME!!!", white)
+Font.print(robotofont,x , y, "He is comming SAVE ME!!!", white)
 y=y+q
 	if Controls.check(Controls.read(), SCE_CTRL_START) then
 		System.exit()
 	end
 	if Controls.check(Controls.read(), SCE_CTRL_CROSS) then
-    Font.print(fontb,x ,y, "I said dont WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYY", white)
+    Font.print(robotofont,x ,y, "I said dont WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYY", white)
     y=y+q
-		Font.print(fontb,x ,y, tostring(System.getUsername()), white)
+		Font.print(robotofont,x ,y, tostring(System.getUsername()), white)
     y=y+q
 		end
 
